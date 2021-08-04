7eadb9d29046acad430a76b9ac898ee54e757af2
JaygoliM <golidjay000@warren.k12.in.us>
2021-08-04 12:51:44 -0400

    completed l1 and l2 notes

--- src/L1.java
+++ src/L1.java
@@ -1,8 +1,15 @@
-public class L1 {
+public class L1{
 
+        public static void main(String[] args){
+            System.out.println("Adios World!!!");
+            System.out.print("Adios World!!!");
+            System.out.print("Adios World!!!");
+            System.out.print("bamos");
+        }
+    }
     //You will notice that the class header is already define.
 
     //add the main method header below.
 
 
-}
+
